7abfeab29e64073222ffb99f2a918c4d6c3c97f5
gitmal <pnirmal896@gmail.com>
2024-09-17 19:59:20 +0530

    add sky information and update code

--- backend.py
+++ backend.py
@@ -1,23 +1,18 @@
 import requests
 
 api_key = 'fd3e75819924aacc7af4bc398df85432'
-def get_data(place , day=None , data=None):
+def get_data(place='tokyo' , day='1', api_key):
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={api_key}'
-    temp =[]
-    date =[]
     response = requests.get(url)
     data = response.json()
     data = data['list']
-    first_date = int(data[0]['dt_txt'].split(' ')[0].split('-')[2])
-    last_date = first_date + day
-    for i in data:
-        loop_date = int(i['dt_txt'].split(' ')[0].split('-')[2])
-        if loop_date <= last_date :
-            temp.append(i['main']['temp'])
-            date.append(i['dt_txt'])
-        else :
-            break
-    return temp, date
+    data = data[0:8*int(day)]
+
+    temp =[row['main']['temp'] for row in data]
+    date = [row['dt_txt'] for row in data]
+    cloud = [row['weather'][0]['main'] for row in data]
+
+    return temp, date , cloud
 
 if __name__ == '__main__':
-    get_data('tokyo')
+    get_data()
--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import streamlit as st
 import plotly.express as pe
-import pandas as pd
 from backend import get_data
+
+api_key = st.secrets['API_KEY']
 
 st.title('Weather Forecast Dashboard')
 
-place = st.text_input(label='Enter a place' , key = 'place' , placeholder='Example- Chennai')
-days = st.slider(label = 'Select the number of days ' , value = 1 , min_value=1 , max_value=5 , key = 'days')
-data_to_display =  st.selectbox(label='Select data to view ' , options=['Temperature' , 'Sky'] , key = 'data')
+place = st.text_input(label='Enter a place' , key = 'place',
+                      placeholder='Example- Chennai' , value='Tokyo')
+days = st.slider(label = 'Select the number of days ',
+                 value = 1 , min_value=1 , max_value=5 , key = 'days')
+data_to_display =  st.selectbox(label='Select data to view ',
+                                options=['Temperature' , 'Sky'] , key = 'data')
+try:
+    temperature,dates,sky = get_data(place=place,day=days,api_key)
 
-if place :
-    if days == 1 :
-        s=''
-    else :
-        s='s'
+    if days == 1:
+        s = ''
+    else:
+        s = 's'
     st.subheader(f"{data_to_display} for the next {days} day{s} in {place.title()}")
-    temperature,dates=get_data(place=place,day=days)
+    if data_to_display == 'Temperature':
+        fig = pe.line(x=dates, y=temperature, labels={'x': 'Date', 'y': 'Temperature'})
+        st.plotly_chart(fig)
 
-    fig = pe.line(x=dates, y=temperature, labels=('Dates' , 'Temperature'))
-    st.plotly_chart(fig)
+    if data_to_display == 'Sky':
 
+        images = {'Clouds': 'cloudimages/clouds.png', 'Rain':'cloudimages/rain.png',
+                  'Clear': 'cloudimages/clear.png', 'Snow': 'cloudimages/snow.png'}
+        image_paths = [images[condition] for condition in sky]
+        st.image(image_paths, width=150 , caption = dates)
+
+except KeyError:
+    st.warning("Enter a valid city name ")
